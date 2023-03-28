d7127104eed294b8e3244813840d68efa6265a0c
Владислав Горский <vg07102004@gmail.com>
2023-03-28 21:45:06 +0300

    Item removal + refactoring

--- src/script.js
+++ src/script.js
@@ -1,11 +1,29 @@
-let addItem = function() {
-    let userInput = document.getElementById('newNote');
-    if (!userInput.value) {
-        window.alert("Nothing to add");
-        return;
-    }
-    let newItem = document.createElement('li');
-    newItem.appendChild(document.createTextNode(userInput.value));
-    document.getElementById('list').appendChild(newItem);
-    userInput.value = '';
+let addItem = function () {
+  let userInput = document.getElementById("newNote");
+  if (!userInput.value) {
+    window.alert("Nothing to add");
+    return;
+  }
+  let newItem = document.createElement("li");
+  newItem.className = "item";
+  newItem.appendChild(document.createTextNode(userInput.value));
+  document.getElementById("list").appendChild(newItem);
+  userInput.value = "";
+  updateRemoveButtonFor(newItem);
+  updateCheckboxFor(newItem);
+};
+
+let updateRemoveButtonFor = function (currentElement) {
+  let clickableSpan = document.createElement("span");
+  clickableSpan.className = "close";
+  clickableSpan.appendChild(document.createTextNode("\u00D7"));
+  clickableSpan.onclick = function () {
+    let currentItem = this.parentElement;
+    currentItem.remove();
+  };
+  currentElement.appendChild(clickableSpan);
+}
+
+let updateCheckboxFor = function (currentElement) {
+
 }
