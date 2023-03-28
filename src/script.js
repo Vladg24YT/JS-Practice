6e6bad65825b6747664eafdc10c8a58042b6eb71
Владислав Горский <vg07102004@gmail.com>
2023-03-28 12:45:10 +0300

    backup

--- src/script.js
+++ src/script.js
@@ -1,11 +1,11 @@
 let addItem = function() {
-    let userInput = document.getElementById('newNote').value;
-    if (!userInput) {
+    let userInput = document.getElementById('newNote');
+    if (!userInput.value) {
         window.alert("Nothing to add");
         return;
     }
     let newItem = document.createElement('li');
-    newItem.appendChild(document.createTextNode(userInput));
+    newItem.appendChild(document.createTextNode(userInput.value));
     document.getElementById('list').appendChild(newItem);
     userInput.value = '';
 }
