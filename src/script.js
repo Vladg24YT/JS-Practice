1a4dbbeeef549fbe4a71fe9eeb7289f72ef59990
Владислав Горский <vg07102004@gmail.com>
2023-04-03 22:32:40 +0300

    Close #1, close #2

--- src/script.js
+++ src/script.js
@@ -1,37 +1,56 @@
-let addItem = function () {
-  let userInput = document.getElementById("newNote");
-  if (!userInput.value) {
-    window.alert("Nothing to add");
-    return;
-  }
-  let newItem = document.createElement("li");
+//update elements for all pre-existing list items
+const itemArray = document.getElementsByTagName("li");
+for (let i = 0; i < itemArray.length; i++) {
+  updateRemoveButtonFor(itemArray[i]);
+  updateCheckboxFor(itemArray[i]);
+}
+
+const checkInput = () => {
+  const userInput = document.getElementById("newNote");
+  if (!userInput.value) { showError("Nothing to add"); }
+  return userInput.value != null;
+}
+
+const addItem = () => {
+  if (!checkInput()) { return; }
+  const newItem = document.createElement("li");
   newItem.className = "item";
-  newItem.appendChild(document.createTextNode(userInput.value));
+  newItem.textContent = userInput.value;
   document.getElementById("list").appendChild(newItem);
   userInput.value = "";
+
   updateRemoveButtonFor(newItem);
   updateCheckboxFor(newItem);
 };
 
-let updateRemoveButtonFor = function (currentElement) {
-  let clickableSpan = document.createElement("span");
+const updateRemoveButtonFor = (currentElement) => {
+  const clickableSpan = document.createElement("span");
+
   clickableSpan.className = "close";
   clickableSpan.appendChild(document.createTextNode("\u00D7"));
-  clickableSpan.onclick = function () {
-    let currentItem = this.parentElement;
-    currentItem.remove();
+  clickableSpan.onclick = () => {
+    this.parentElement.remove();
   };
+
   currentElement.appendChild(clickableSpan);
 }
 
-let updateCheckboxFor = function (currentElement) {
-  let clickableBox = document.createElement("span");
+const updateCheckboxFor = (currentElement) => {
+  const clickableBox = document.createElement("span");
   clickableBox.className = "checkbox";
+
   currentElement.appendChild(clickableBox);
-  currentElement.onclick = function(e) {
-    console.log(e.target.tagName);
+  currentElement.onclick = (e) => {
     if (e.target.tagName === "LI") {
       e.target.classList.toggle('checked');
     }
   }
+}
+
+const showError = (message) => {
+  window.alert(message);
+  /*const alertFrame = document.createElement("div");
+  alertFrame.className = "alert";
+  alertFrame.textContent = message;
+  document.getElementById("body").appendChild(alertFrame);*/
 }
